8555e1011a64e7027334b0c6835755fc9e269b41
Shoumyo <shoumyoarchit@gmail.com>
2018-09-08 19:25:38 -0400

    Fixed profile updating

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -10,6 +10,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
     declarations: [
@@ -24,7 +25,8 @@ import { MatButtonModule } from '@angular/material/button';
       MatFormFieldModule,
       MatInputModule,
       MatSelectModule,
-      MatButtonModule
+      MatButtonModule,
+      MatSnackBarModule
     ],
     exports: [DashboardComponent]
   })
