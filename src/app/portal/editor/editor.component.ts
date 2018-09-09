6b679a5bbf179539db0dbeecf43fe8a0fe1d4042
Pranay Neelagiri <pneeldam@gmail.com>
2018-09-09 01:20:51 -0400

    Add colloborative editing

--- src/app/portal/editor/editor.component.ts
+++ src/app/portal/editor/editor.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { AceEditorComponent } from 'ng2-ace-editor';
+import { WebsocketService } from '../../core/websocket/websocket.service';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnInit, AfterViewInit {
+export class EditorComponent implements OnInit {
 
   text = '';
+  hasUpdated = false;
+
+  syncedString: string;
 
   @ViewChild('editor') editor: AceEditorComponent;
 
-  constructor() { }
+  constructor(private websocketService: WebsocketService) { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-
+    this.websocketService.getSocket().on('addEditor', (data) => {
+      const text = data.data;
+      this.text = text;
+    });
   }
 
   onChange(code) {
+    this.websocketService.emit('sendEditor', code);
     console.log(code);
   }
 
