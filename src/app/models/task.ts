7260df025ba8dafb22f1190ec3cbfbb6dad25483
akench <atharva.kench@gmail.com>
2018-09-08 21:33:10 -0400

    started task list

---
+++ src/app/models/task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+
+    completed: boolean;
+    name: string;
+
+}
--- src/app/portal/task-list/task-list.component.ts
+++ src/app/portal/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../models/task';
 
 @Component({
   selector: 'app-task-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListComponent implements OnInit {
 
+  tasks: Task[] = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    this.tasks = [{completed: false,  name: 'task 1'}, {completed: false, name: 'task 2'}];
+
+  }
+
+  toggleCompleted(i: number) {
+
+    this.tasks[i].completed = !this.tasks[i].completed;
+    console.log(i + ' was clicked');
   }
 
 }
